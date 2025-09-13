b727502f9c92dd31cbc4777c445c7708ee8cba71
ChandanPradhan-AI <chandanpradhan.develop@gmail.com>
2025-09-14 03:11:13 +0530

    Complated app ui

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,6 +1,43 @@
 "use client";
+
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
+
 const Clock = () => {
-	return <>this is a digital clock</>;
+	const [hours, setHours] = useState(parseInt("00"));
+	const [minutes, setMinutes] = useState(parseInt("00"));
+	const [seconds, setSeconds] = useState(parseInt("00"));
+	const [meridiem, setMeridiem] = useState("XX");
+	const [date, setDate] = useState(format(new Date(), "eeee, dd LLLL yyyy"));
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setHours(parseInt(format(new Date(), "hh")));
+			setMinutes(parseInt(format(new Date(), "mm")));
+			setSeconds(parseInt(format(new Date(), "ss")));
+			setMeridiem(format(new Date(), "a"));
+
+			setDate(format(new Date(), "eeee, dd LLLL yyyy"));
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+	return (
+		<>
+			<div className="space-y-4">
+				<div className="flex items-center gap-1 font-mono text-9xl">
+					<div className="">{hours}</div>{" "}
+					<span className="animate-pulse">:</span>
+					<div className="">{minutes}</div>
+					<span className="animate-pulse">:</span>
+					<div className="">{seconds}</div>
+					<span className="animate-pulse">:</span>
+					<div className="">{meridiem}</div>
+				</div>
+				<div className="text-center text-6xl">{date}</div>
+			</div>
+		</>
+	);
 };
 
 export default Clock;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 import ThemeToggleButton from "../customui/ThemeToggleButton";
-import { AuroraText } from "./aurora-text";
+import { AuroraText } from "../magicui/aurora-text";
 
 const Header = () => {
 	return (
 		<header
-			className="border-b shadow"
+			className="bg-background/10 absolute top-0 right-0 left-0 border-b border-white/10 backdrop-blur"
 			aria-label="app-header">
 			<div className="container mx-auto flex items-center justify-between px-6 py-3">
 				<Link href={"/"}>
 					<h1
-						className="text-2xl font-semibold"
+						className="text-3xl font-bold"
 						aria-label="App Name">
 						<AuroraText> Digial Clock</AuroraText>
 					</h1>
